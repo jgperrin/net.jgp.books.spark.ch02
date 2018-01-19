e73dd4224bbb5c328a78452adc4fa8242f43c84b
Jean Georges Perrin <jgp@jgp.net>
2018-01-18 21:01:06 -0600

    Full example

--- src/main/java/net/jgp/books/sparkWithJava/ch02/CsvToRelationalDatabaseApp.java
+++ src/main/java/net/jgp/books/sparkWithJava/ch02/CsvToRelationalDatabaseApp.java
@@ -1,7 +1,13 @@
 package net.jgp.books.sparkWithJava.ch02;
 
+import static org.apache.spark.sql.functions.concat;
+import static org.apache.spark.sql.functions.lit;
+
+import java.util.Properties;
+
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
 
 /**
@@ -11,10 +17,11 @@ import org.apache.spark.sql.SparkSession;
  */
 public class CsvToRelationalDatabaseApp {
 
-	/**
-	 * main() is your entry point to the application. 
-	 * @param args
-	 */
+    /**
+     * main() is your entry point to the application.
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         CsvToRelationalDatabaseApp app = new CsvToRelationalDatabaseApp();
         app.start();
@@ -25,17 +32,37 @@ public class CsvToRelationalDatabaseApp {
      */
     private void start() {
         // Creates a session on a local master
-        SparkSession spark = SparkSession.builder()
-                .appName("CSV to DB")
-                .master("local")
-                .getOrCreate();
+        SparkSession spark = SparkSession.builder().appName("CSV to DB").master("local").getOrCreate();
+
+        // Step 1: Ingestion
+        // ---------
 
         // Reads a CSV file with header, called books.csv, stores it in a dataframe
-        Dataset<Row> df = spark.read().format("csv")
-                .option("header", "true")
-                .load("data/books.csv");
+        Dataset<Row> df = spark.read().format("csv").option("header", "true").load("data/authors.csv");
 
-        // Shows at most 5 rows from the dataframe
-        df.show(5);
+        // Step 2: Transform
+        // ---------
+
+        // Creates a new column called "name" as the concatenation of lname, a virtual
+        // column containing ", " and the fname column
+        df = df.withColumn("name", concat(df.col("lname"), lit(", "), df.col("fname")));
+
+        // Step 3: Save
+        // ----
+
+        // The connection URL, assuming your PostgreSQL instance runs locally on the
+        // default port, and the database we use is "spark_labs"
+        String dbConnectionUrl = "jdbc:postgresql://localhost/spark_labs";
+
+        // Properties to connect to the database, the JDBC driver is part of our pom.xml
+        Properties prop = new Properties();
+        prop.setProperty("driver", "org.postgresql.Driver");
+        prop.setProperty("user", "jgp");
+        prop.setProperty("password", "Spark<3Java");
+
+        // Write in a table called ch02
+        df.write().mode(SaveMode.Overwrite).jdbc(dbConnectionUrl, "ch02", prop);
+
+        System.out.println("Process complete");
     }
 }
