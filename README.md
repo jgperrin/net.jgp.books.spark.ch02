01974f0d6a1de78a830fe50f8374192e2505df91
jgp <jgp@jgp.net>
2019-12-25 09:51:18 -0500

    Added formatter

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
-The examples in this repository are support to the **[Spark in Action, 2nd edition](http://jgp.net/sia)** book by Jean Georges Perrin and published by Manning. Find out more about the book on [Manning's website](http://jgp.net/sia).
+This repository contains the Java labs as well as their Scala and Python ports of the code used in Manning Publication’s **[Spark in Action, 2nd edition](https://www.manning.com/books/spark-in-action-second-edition?a_aid=jgp)**, by Jean-Georges Perrin.
+
+---
 
 # Spark in Action, 2nd edition - chapter 2
 
 Welcome to Spark with Java, chapter 2. As we are building our mental model around Spark processing, we need a small but efficient example that does a whole flow.
+
+## Lab
+
+Each chapter has one or more labs. Labs are examples used for teaching in the book(https://www.manning.com/books/spark-in-action-second-edition?a_aid=jgp). You are encouraged to take ownership of the code and modify it, experiment with it, hence the use of the term **lab**.
+
+### Lab \#100
+
+The `CsvToDatabaseApp` application does the following:
+
+ 1.      It acquires a session (a `SparkSession`).
+ 1.      It asks Spark to load (ingest) a dataset in CSV format.
+ 1. Spark performs a small transformation.
+ 1.      Spark stores the result in a database, in this example it is PostgreSQL.
+
+### Lab \#110
+
+The `CsvToApacheDerbyApp` application is similar to lab \#100, however, instead of using PostgreSQL, it uses [Apache Derby](https://db.apache.org/derby/).
+
+Special thanks to Kelvin Rawls for porting the code to Derby. 
+
+> Apache Derby has a bit of a special place in my heart. Originally named Cloudscape, the eponym company was acquired by [Informix](https://en.wikipedia.org/wiki/IBM_Informix) in 1999, which was itself acquired by [IBM](https://en.wikipedia.org/wiki/IBM) in 2001. Informix's project Arrowhead had the ambition to align the SQL (and other features) between all their databases. Under the new ownership, Cloudscape was influenced by the Db2 SQL before going to the Open Source community as Apache Derby.
 
 ## Running the lab in Java
 
@@ -18,18 +41,16 @@ You will need:
  * Apache Spark (please refer Appendix P - 'Spark in production: installation and a few tips').
 
 1. Clone this project
-    
-   git clone https://github.com/jgperrin/net.jgp.books.spark.ch02
+
+    git clone https://github.com/jgperrin/net.jgp.books.spark.ch02
 
 2. Go to the lab in the Python directory
-    
-   cd net.jgp.books.spark.ch02/src/main/python/lab100_csv_to_db/
+
+    cd net.jgp.books.spark.ch02/src/main/python/lab100_csv_to_db/
 
 3. Execute the following spark-submit command to create a jar file to our this application
        
-   ```
-     spark-submit csvToRelationalDatabaseApp.py
-   ```
+    spark-submit csvToRelationalDatabaseApp.py
    
 ## Running the lab in Scala
 
@@ -41,22 +62,20 @@ You will need:
 
 
 1. Clone this project
-       
-   git clone https://github.com/jgperrin/net.jgp.books.spark.ch02
 
-2. cd net.jgp.books.spark.ch02
+    git clone https://github.com/jgperrin/net.jgp.books.spark.ch02
+
+2. Go to the correct directory:
+
+    cd net.jgp.books.spark.ch02
 
 3. Package application using sbt command
 
-   ```
-      sbt clean assembly
-   ```
+    sbt clean assembly
 
 4. Run Spark/Scala application using spark-submit command as shown below:
-   
-    ```
+
     spark-submit --class net.jgp.books.spark.ch02.lab100_csv_to_db.CsvToRelationalDatabaseScalaApp target/scala-2.11/SparkInAction2-Chapter02-assembly-1.0.0.jar
-    ```
 
 Notes: 
  1. [Java] Due to renaming the packages to match more closely Java standards, this project is not in sync with the book's MEAP prior to v10 (published in April 2019).
@@ -64,4 +83,4 @@ Notes:
  
 ---
 
-Follow me on Twitter to get updates about the book and Apache Spark: [@jgperrin](https://twitter.com/jgperrin). Join the book's community on [Facebook](https://www.facebook.com/SparkWithJava/) or in [Manning's community site](https://forums.manning.com/forums/spark-in-action-second-edition?a_aid=jgp).
+Follow me on Twitter to get updates about the book and Apache Spark: [@jgperrin](https://twitter.com/jgperrin). Join the book's community on [Facebook](https://facebook.com/sparkinaction/) or in [Manning's live site](https://forums.manning.com/forums/spark-in-action-second-edition?a_aid=jgp).
---
+++ src/main/java/net/jgp/books/spark/ch02/lab110_csv_to_derby/CsvToApacheDerbyApp.java
@@ -0,0 +1,215 @@
+package net.jgp.books.spark.ch02.lab110_csv_to_derby;
+
+import static org.apache.spark.sql.functions.concat;
+import static org.apache.spark.sql.functions.lit;
+
+import java.net.InetAddress;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import org.apache.derby.drda.NetworkServerControl;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * CSV to a relational database.
+ *
+ * @author jgp
+ */
+public class CsvToApacheDerbyApp {
+
+  private static final String JDBC_DRIVER = "org.apache.derby.jdbc.ClientDriver";;
+
+  /**
+   * main() is your entry point to the application.
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
+    CsvToApacheDerbyApp app = new CsvToApacheDerbyApp();
+
+    String user = "admin";
+    String password = "adminpass";
+    String dbHost = "localhost";
+    int dbPort = 1527;
+    String dbConnectionUrl = "jdbc:derby://" + dbHost + ":" + dbPort + "/spark_labs;create=true";
+
+    try {
+      app.startDatabase(dbHost, dbPort);
+    } catch (Exception e) {
+      System.out.println("Could not initiate database, stopping: " + e.getMessage());
+      return;
+    }
+    app.start(dbConnectionUrl, user, password);
+    app.testDatabase(dbConnectionUrl, user, password);
+  }
+
+  private void startDatabase(String dbHost, int dbPort) throws Exception {
+    // Starts Apache Derby server
+    NetworkServerControl server = new NetworkServerControl(InetAddress.getByName(dbHost), dbPort);
+    server.start(null);
+  }
+
+  /**
+   * The processing code.
+   */
+  private void start(String dbConnectionUrl, String user, String password) {
+    // Creates a session on a local master
+    SparkSession spark = SparkSession.builder()
+        .appName("CSV to DB")
+        .master("local")
+        .getOrCreate();
+
+    // Step 1: Ingestion
+    // ---------
+    // Reads a CSV file with header, called authors.csv, stores it in a
+    // dataframe
+    Dataset<Row> df = spark.read()
+        .format("csv")
+        .option("header", "true")
+        .load("data/authors.csv");
+
+    // Step 2: Transform
+    // ---------
+    // Creates a new column called "name" as the concatenation of lname, a
+    // virtual column containing ", " and the fname column
+    df = df.withColumn(
+        "name",
+        concat(df.col("lname"), lit(", "), df.col("fname")));
+
+    // Step 3: Save
+    // ----
+    // The connection URL, assuming your PostgreSQL instance runs locally on
+    // the
+    // default port, and the database we use is "spark_labs"
+
+    // Properties to connect to the database, the JDBC driver is part of our
+    // pom.xml
+    Properties prop = new Properties();
+    prop.setProperty("driver", JDBC_DRIVER);
+    prop.setProperty("user", user);
+    prop.setProperty("password", password);
+
+    // Write in a table called ch02
+    df.write()
+        .mode(SaveMode.Overwrite)
+        .jdbc(dbConnectionUrl, "ch02", prop);
+
+    System.out.println("Load Database Process complete");
+  }
+
+  private void testDatabase(String dbUrl, String dbUser, String dbPassword) {
+    Connection conn = null;
+    Statement stmt = null;
+    try {
+      // STEP 2: Register JDBC driver
+      Class.forName(JDBC_DRIVER);
+
+      // STEP 3: Open a connection
+      System.out.println("Connecting to database...");
+      conn = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+
+      // STEP 4: Execute a query
+      System.out.println("Creating statement...");
+      stmt = conn.createStatement();
+      String sql;
+      sql = "SELECT * FROM ch02";
+      // STEP 5: Extract data from result set
+      try (ResultSet rs = stmt.executeQuery(sql)) {
+        printResults(rs);
+        printResults2(rs);
+        // STEP 6: Clean-up environment
+      }
+      stmt.close();
+      conn.close();
+    } catch (SQLException | ClassNotFoundException se) {
+      // Handle errors for JDBC
+      System.out.println("Errors " + se.getMessage());
+    } // Handle errors for Class.forName
+    finally {
+      // finally block used to close resources
+      try {
+        if (stmt != null) {
+          stmt.close();
+        }
+      } catch (SQLException se2) {
+      } // nothing we can do
+      try {
+        if (conn != null) {
+          conn.close();
+        }
+      } catch (SQLException se) {
+        System.out.println("Errors " + se.getMessage());
+      } // end finally try
+    } // end try
+    System.out.println("Test Database Complete");
+  }// end TestDatabase
+
+  /**
+   * Prints the results from the resultset
+   * 
+   * @param rs
+   * @throws SQLException
+   */
+  private void printResults(ResultSet rs) throws SQLException {
+    ArrayList<String> columnNames = new ArrayList<>();
+    if (rs != null) {
+      ResultSetMetaData columns = rs.getMetaData();
+      int i = 0;
+      while (i < columns.getColumnCount()) {
+        i++;
+        System.out.print(columns.getColumnName(i) + "\t\t");
+        columnNames.add(columns.getColumnName(i));
+      }
+      System.out.print("\n");
+
+      while (rs.next()) {
+        for (i = 0; i < columnNames.size(); i++) {
+          System.out.print(rs.getString(columnNames.get(i))
+              + "\t\t");
+
+        }
+        System.out.print("\n");
+      }
+    }
+  }
+
+  /**
+   * Prints the results from the resultset
+   * 
+   * @param rs
+   * @throws SQLException
+   */
+  private void printResults2(ResultSet rs) throws SQLException {
+    ArrayList<String> columnNames = new ArrayList<>();
+    if (rs != null) {
+      ResultSetMetaData columns = rs.getMetaData();
+      int i = 0;
+      while (i < columns.getColumnCount()) {
+        i++;
+        System.out.print(columns.getColumnName(i) + "\t\t");
+        columnNames.add(columns.getColumnName(i));
+      }
+      System.out.print("\n");
+
+      while (rs.next()) {
+        for (i = 0; i < columnNames.size(); i++) {
+          System.out.print(rs.getString(columnNames.get(i))
+              + "\t\t");
+
+        }
+        System.out.print("\n");
+      }
+
+    }
+
+  }
+}
